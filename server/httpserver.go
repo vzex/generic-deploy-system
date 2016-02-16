dbb2bab7e40b1c690034fb1f13fdd46d0eed7f4d
vzex <vzex@163.com>
2016-02-16 18:10:58 +0800

    update

--- server/httpserver.go
+++ server/httpserver.go
@@ -1,23 +1,23 @@
 package server
 import "net/http"
-import "net"
+import "html/template"
 import "log"
-import "strconv"
 
-func InitAdminPort(port int) error {
-        mux := http.NewServeMux()
-        f := func(w http.ResponseWriter, r *http.Request) {
-        }
-        mux.HandleFunc("/admin", f)
-        addr := "0.0.0.0:" + strconv.Itoa(port)
-        server := &http.Server{Addr: addr, Handler: mux}
-        listener, err := net.Listen("tcp", addr)
-        if err != nil {
-                return err 
-        }   
-        log.Println("begin serve admin port:", addr)
-        go server.Serve(listener)
-        return nil 
+func InitAdminPort(addr, websock string) error {
+        http.Handle("/css/", http.FileServer(http.Dir("website")))
+        http.Handle("/fonts/", http.FileServer(http.Dir("website")))
+        http.Handle("/js/", http.FileServer(http.Dir("website")))
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+                t, er := template.ParseFiles("website/index.html")
+                if er == nil {
+                        t.Execute(w, websock)
+                } else {
+                        log.Println("err:", er.Error())
+                }
+        })
+        log.Println("begin serve http:", addr)
+        er := http.ListenAndServe(addr, nil)
+        return er
 }
 
 
--- server/init.go
+++ server/init.go
@@ -1,11 +1,17 @@
 package server
 
 import "flag"
+import "log"
 
 var service = flag.String("service", "", "for remote client connect")
 var webservice = flag.String("web", ":8080", "http server port")
-var websocket = flag.String("websocket", ":8081", "http web-socket port")
+var websocket = flag.String("websocket", "10.240.160.17:8081", "http web-socket port")
 func Init() {
 	flag.Parse()
+        er:=InitAdminPort(*webservice, *websocket)
+        if er !=nil {
+                log.Println("web server fail", er.Error())
+                return
+        }
 	Listen(*service)
 }
