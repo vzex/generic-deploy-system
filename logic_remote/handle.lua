f0ac839ef8bd7f1d699604c7967ddc13196c074a
vzex <vzex@163.com>
2016-03-02 18:00:00 +0800

    update

--- logic_remote/handle.lua
+++ logic_remote/handle.lua
@@ -1,7 +1,2 @@
 
-function handle_cmd(info)
-        local commond = info.Cmd
-        local s, ok = cmd(commond)
-        SendBack(MsgPack.pack({str=s, ok = ok}))
-end
 
--- logic_remote/internal/init.lua
+++ logic_remote/internal/init.lua
@@ -1,2 +1,7 @@
 local m = require "logic/internal/pack"
+function handle_cmd(info)
+        local commond = info.Cmd
+        local s, ok = cmd(commond)
+        SendBack(MsgPack.pack({str=s, ok = ok}))
+end
 return m
